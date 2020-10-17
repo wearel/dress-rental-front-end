39d7d4f3177fd7f19e328586f40e909019e0102b
azezmohammed <muhamadaziz047@gmail.com>
2020-10-17 10:32:25 +0700

    new update header

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,45 @@
-import React from 'react'
+import React from "react";
+// import { Link } from "react-router-dom";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import "fontsource-roboto";
+
 // Styles Css
-import "./Header.css"
+import "./Header.css";
 
 function Header() {
-    return (
-        <div>
-            <h1>Ini halaman Header</h1>
-        </div>
-    )
+  return (
+    <div>
+      <AppBar>
+        <Toolbar>
+          <Typography>Home</Typography>
+          <Typography>Product</Typography>
+          <Typography>About Us</Typography>
+          <Button>Login</Button>
+          <Button>Register</Button>
+        </Toolbar>
+      </AppBar>
+      {/* <ul>
+        <li>
+          <Link to="/">Home</Link>
+        </li>
+        <li>
+          <Link to="/product">Product</Link>
+        </li>
+        <li>
+          <Link to="/about">About Us</Link>
+        </li>
+        <li>
+          <Link to="/login">Login</Link>
+        </li>
+        <li>
+          <Link to="/register">Register</Link>
+        </li>
+      </ul> */}
+    </div>
+  );
 }
 
-export default Header
+export default Header;
