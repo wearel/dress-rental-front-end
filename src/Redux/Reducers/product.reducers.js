347b735ff8ff5888aeaa6acf47b70c031ea84f05
azezmohammed <muhamadaziz047@gmail.com>
2020-10-19 15:08:32 +0700

    buat redux untuk product

--- src/Redux/Reducers/product.reducers.js
+++ src/Redux/Reducers/product.reducers.js
@@ -1,4 +1,8 @@
-import { GET_PRODUCT } from "../Actions/product.action";
+import {
+  GET_PRODUCT_REQUEST,
+  GET_PRODUCT_SUCCESS,
+  GET_PRODUCT_FAILED,
+} from "../Actions/product.action";
 
 const intialState = {
   data: [],
@@ -8,10 +12,22 @@ const intialState = {
 
 export function getProductReducers(state = intialState, action) {
   switch (action.type) {
-    case GET_PRODUCT:
+    case GET_PRODUCT_REQUEST:
       return {
         ...state,
         isLoading: true,
+      };
+    case GET_PRODUCT_SUCCESS:
+      return {
+        ...state,
+        isLoading: false,
+        data: action.result,
+      };
+    case GET_PRODUCT_FAILED:
+      return {
+        ...state,
+        isloading: false,
+        error: action.error,
       };
     default:
       return state;
