9e66d76f88f48dff1480751641a5629a260c582e
Sarah Julia Gushef <sarahgushef@gmail.com>
2020-10-23 17:59:46 +0700

    fix get all product action

--- src/Redux/Actions/product.action.js
+++ src/Redux/Actions/product.action.js
@@ -1,70 +1,55 @@
-import axios from "axios";
-export const GET_ALL_PRODUCTS_REQUEST = "GET_ALL_PRODUCTS_REQUEST";
-export const GET_ALL_PRODUCTS_SUCCESS = "GET_ALL_PRODUCTS_SUCCESS";
-export const GET_ALL_PRODUCTS_FAILED = "GET_ALL_PRODUCTS_FAILED";
-export const GET_PRODUCT_DETAILS_REQUEST = "GET_PRODUCTS_DETAILS_REQUEST";
-export const GET_PRODUCT_DETAILS_SUCCESS = "GET_PRODUCTS_DETAILS_SUCCESS";
-export const GET_PRODUCT_DETAILS_FAILED = "GET_PRODUCTS_DETALS_FAILED";
-
-export const getAllProductRequest = () => {
-  return {
-    type: GET_ALL_PRODUCTS_REQUEST,
-  };
-};
-
-export const getAllProductSuccess = (result) => {
-  return {
-    type: GET_ALL_PRODUCTS_SUCCESS,
-    payload: result,
-  };
-};
-
-export const getAllProductFailed = (error) => {
-  return {
-    type: GET_ALL_PRODUCTS_FAILED,
-    payload: error,
-  };
-};
-export const getProductDetailRequest = () => {
-  return {
-    type: GET_PRODUCT_DETAILS_REQUEST,
-  };
-};
-
-export const getProductDetailSuccess = (result) => {
-  return {
-    type: GET_PRODUCT_DETAILS_SUCCESS,
-    payload: result,
-  };
-};
-
-export const getProuctDetailFailed = (error) => {
-  return {
-    type: GET_PRODUCT_DETAILS_FAILED,
-    payload: error,
-  };
-};
+import axios from 'axios';
+export const GET_ALL_PRODUCTS_REQUEST = 'GET_ALL_PRODUCTS_REQUEST';
+export const GET_ALL_PRODUCTS_SUCCESS = 'GET_ALL_PRODUCTS_SUCCESS';
+export const GET_ALL_PRODUCTS_FAILED = 'GET_ALL_PRODUCTS_FAILED';
+export const GET_PRODUCT_DETAILS_REQUEST = 'GET_PRODUCTS_DETAILS_REQUEST';
+export const GET_PRODUCT_DETAILS_SUCCESS = 'GET_PRODUCTS_DETAILS_SUCCESS';
+export const GET_PRODUCT_DETAILS_FAILED = 'GET_PRODUCTS_DETAILS_FAILED';
 
 export const getProductAction = () => {
-  return async (dispatch) => {
-    const response = await axios
-      .get("https://5f51a6945e98480016123be3.mockapi.io/products")
-      .then((result) => dispatch(getAllProductSuccess(result.data)))
-      .catch((error) => dispatch(getAllProductFailed(error)));
+    return async (dispatch) => {
+        try {
+            dispatch({
+                type: GET_ALL_PRODUCTS_REQUEST,
+            });
 
-    dispatch(getAllProductRequest(response.data));
-    console.log("Hasil get data", response);
-  };
+            const response = await axios.get(
+                'https://5f51a6945e98480016123be3.mockapi.io/products'
+            );
+
+            dispatch({
+                type: GET_ALL_PRODUCTS_SUCCESS,
+                payload: response.data,
+            });
+        } catch (error) {
+            dispatch({
+                type: GET_ALL_PRODUCTS_FAILED,
+                payload: error,
+            });
+        }
+    };
 };
 
 export const getProductById = (id) => {
-  return async (dispatch) => {
-    const response = await axios
-      .get(`https://5f51a6945e98480016123be3.mockapi.io/products/${id}`)
-      .then((result) => dispatch(getProductDetailSuccess(result.data)))
-      .catch((error) => dispatch(getAllProductFailed(error)));
+    return async (dispatch) => {
+        try {
+            dispatch({
+                type: GET_PRODUCT_DETAILS_REQUEST,
+            });
 
-    dispatch(getAllProductRequest(response.data));
-    console.log("hasil detail", response);
-  };
+            const response = await axios.get(
+                `https://5f51a6945e98480016123be3.mockapi.io/products/${id}`
+            );
+
+            dispatch({
+                type: GET_PRODUCT_DETAILS_SUCCESS,
+                payload: response.data,
+            });
+        } catch (error) {
+            dispatch({
+                type: GET_PRODUCT_DETAILS_FAILED,
+                payload: error,
+            });
+        }
+    };
 };
